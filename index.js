9b92310186b064e72fed0119d56df5f9ea5d95d9
yousif <yousifr.ismail@gmail.com>
2022-10-24 23:26:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
 }
+const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
